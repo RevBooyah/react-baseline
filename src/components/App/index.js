176bb10000c98e74f35c46deca44b1aca3c90891
dbrudnicki <dbrudnicki@nmetric.com>
2018-12-11 10:13:03 -0800

    initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import './style.scss'
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    )
+  }
+}
